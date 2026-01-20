d96f3d865495062b38982a13585b1eddc3a52e09
Nishant-Pall <nishantpall877@gmail.com>
2026-01-20 23:20:42 +0530

    change variables for better viewing

--- main.go
+++ main.go
@@ -9,24 +9,24 @@ import (
 
 func main() {
 
-	brep := binary.NewBinaryRep(6553)
+	brep := binary.NewBinaryRep(65535)
 
 	bitSumArr := brep.GetBitSumArray()
 
-	const S = 10240
+	const S = 20240
 	dc := gg.NewContext(S, S)
 	dc.SetRGB(0, 0, 0)
 	dc.Clear()
 	dc.Translate(S/2, S/2)
 	dc.Scale(40, 40)
-	dc.SetLineWidth(4)
+	dc.SetLineWidth(2)
 	dc.SetRGBA(0, 1, 0, 0.4)
 
 	var x, y float64
-	x, y = 0, 100
+	x, y = 0, 200
 
-	dx := 1 / math.Sqrt(2)
-	dy := 1 / math.Sqrt(2)
+	dx := 1 / (math.Sqrt(2) * 3)
+	dy := 1 / (math.Sqrt(2) * 3)
 
 	dc.MoveTo(x, y)
 	rad := 60 * math.Pi / 180
